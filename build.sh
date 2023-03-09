a852d073d0580092d01cdfd482dd8e5ddfdd9c67
tsenoner <tobias.senoner94@gmail.com>
2023-03-09 16:27:53 +0100

    update build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit
 # create requirements.txt from pyproject.toml and install with pip
 poetry export -f requirements.txt --output requirements.txt
 pip install -r requirements.txt
-poetry install
+# poetry install
