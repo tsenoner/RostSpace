dbbdab0f9bbbfc9a6ff1ce5ba8ede6fd4921ae18
tsenoner <senoner@rostlab.org>
2023-03-09 11:50:19 +0100

    Create build.sh to run on render

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# create requirements.txt from pyproject.toml and install with pip
+poetry export -f requirements.txt --output requirements.txt
+pip install -r requirements.txt
